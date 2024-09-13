e392d85d76b67683938704ae2a83011a75b502bc
Alan <28246519+axpk@users.noreply.github.com>
2024-09-13 01:58:53 -0700

    Added argument parsing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main(int argc, char* argv[]) {
+    std::vector<std::string> vmFiles;
+    for (int i = 0; i < argc; i++) {
+        std::string arg = argv[i];
+        std::cout << arg << std::endl;
+        if (arg == "-v" && i + 1 < argc) {
+            vmFiles.push_back(argv[i++]);
+        } else {
+            std::cerr << "Invalid arguments, placed a -v without a following file" << arg << std::endl;
+            return 1;
+        }
+    }
+
+    if (vmFiles.empty()) {
+        std::cerr << "No VM files provided. Use -v to specify files." << std::endl;
+        return 1;
+    }
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
