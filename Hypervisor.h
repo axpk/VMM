59cffb30fe12ebb9953409185794b6244c3a1406
Alan <28246519+axpk@users.noreply.github.com>
2024-09-14 16:04:00 -0700

    Added Hypervisor class files

---
+++ Hypervisor.h
@@ -0,0 +1,12 @@
+#ifndef VMM_HYPERVISOR_H
+#define VMM_HYPERVISOR_H
+
+#include <vector>
+#include <string>
+
+class Hypervisor {
+    std::vector<std::string> vms;
+};
+
+
+#endif //VMM_HYPERVISOR_H
--- main.cpp
+++ main.cpp
@@ -2,22 +2,25 @@
 #include <string>
 #include <vector>
 #include "main.h"
+#include "Hypervisor.h"
 
 int main(int argc, char* argv[]) {
+
     std::vector<std::string> vmFiles;
+    std::string configFile;
+
     for (int i = 1; i < argc; i++) {
         std::string arg = argv[i];
         std::cout << arg << std::endl;
-        if (arg == "-v" && i + 1 < argc) {
-            vmFiles.push_back(argv[i++]);
-        } else {
-            std::cerr << "Invalid arguments, placed a -v without a following file" << arg << std::endl;
-            return 1;
+        if (arg == "-v" && i + 1 < argc) { // VM Files
+            vmFiles.push_back(argv[++i]);
+        } else if (arg == "-c" && i + 1 < argc) { // Config file
+            configFile = argv[++i];
         }
     }
 
-    if (vmFiles.empty()) {
-        std::cerr << "No VM files provided. Use -v to specify files." << std::endl;
+    if (vmFiles.empty() || configFile.empty()) {
+        std::cerr << "No VM files or config provided. Use -v and -c to specify files." << std::endl;
         return 1;
     }
 
