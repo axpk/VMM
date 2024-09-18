bcef74e7bfcda88e79a15aa18ec0804d6eca55e8
Alan Park <28246519+axpk@users.noreply.github.com>
2024-09-18 00:08:17 -0700

    Added example input files

--- main.cpp
+++ main.cpp
@@ -7,20 +7,17 @@
 int main(int argc, char* argv[]) {
 
     std::vector<std::string> vmFiles;
-    std::string configFile;
 
     for (int i = 1; i < argc; i++) {
         std::string arg = argv[i];
         std::cout << arg << std::endl;
         if (arg == "-v" && i + 1 < argc) { // VM Files
             vmFiles.push_back(argv[++i]);
-        } else if (arg == "-c" && i + 1 < argc) { // Config file
-            configFile = argv[++i];
         }
     }
 
-    if (vmFiles.empty() || configFile.empty()) {
-        std::cerr << "No VM files or config provided. Use -v and -c to specify files." << std::endl;
+    if (vmFiles.empty()) {
+        std::cerr << "No VM files provided. Use -v to specify files." << std::endl;
         return 1;
     }
 
