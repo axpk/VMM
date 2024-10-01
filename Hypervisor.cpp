2b00a8af0884ff2a175e7f3719b90a69b3cb8122
Alan <28246519+axpk@users.noreply.github.com>
2024-10-01 08:52:58 -0700

    Deleted unneeded files

--- Hypervisor.cpp
+++
@@ -1,7 +0,0 @@
-#include "Hypervisor.h"
-
-void Hypervisor::createVM(const Config& config) {
-    std::unique_ptr<VM> vm = std::make_unique<VM>(config);
-    vms.push_back(std::move(vm));
-}
-
--- VM.cpp
+++
@@ -1,7 +0,0 @@
-#include "VM.h"
-
-VM::VM(const Config &config) {
-    this->config = config;
-    this->cpu = std::make_unique<CPU>();
-    parseAssemblyFile(this->config.vm_binary, assemblyLines);
-}
